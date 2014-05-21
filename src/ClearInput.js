34d1d0cff8155d3f86afb4f946246d9b98e80188
sun2rise <alessandro.annini@gmail.com>
2014-05-21 15:11:47 +0200

    FIX - events
    events fixed + added license

--- src/ClearInput.js
+++ src/ClearInput.js
@@ -1,5 +1,114 @@
 /*
+ */*
  *
+ *   The MIT License (MIT)
+ *
+ *   Copyright (c) 2014 Alessandro Filippo Annini
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ * Owner: alessandro.annini@gmail.com
+ * @license MIT
+ * @copyright Alessandro Filippo Annini, 2014
+ *
+* */
+
+define(function(require, exports, module) {
+    var View = require('famous/core/View');
+    var Surface = require('famous/core/Surface');
+    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var InputSurface = require('famous/surfaces/InputSurface');
+    var StateModifier = require('famous/modifiers/StateModifier');
+    var Transform = require('famous/core/Transform');
+    var EventHandler = require('famous/core/EventHandler');
+
+    function ClearInput(options) {
+        View.apply(this, arguments);
+
+        this.eventOutput = new EventHandler();
+        EventHandler.setOutputHandler(this, this.eventOutput);
+
+        this.rootModifier = new StateModifier({
+            size: this.options.size
+        });
+
+        this._mainSurface = new Surface({
+            size: options.size || [undefined, true],
+            classes: options.classes || ['clear-input']
+        });
+
+        this._inputSurface = new InputSurface({
+            size: this._mainSurface.getSize(),
+            placeholder: options.placeholder,
+            type: 'text'
+        });
+
+        this._imageSurface = new ImageSurface({
+            size: [this._inputSurface.getSize()[1], this._inputSurface.getSize()[1]],
+            content: options['clearImage'] || '<img src="' + defaultClearImg + '"/>',
+            properties: {
+                paddingRight: '4px'
+            }
+        });
+
+        var scale = Transform.scale(0.8, 0.8, 0);
+        var translate = Transform.translate(-4, 0, 0);
+        this._imageModifier = new StateModifier({
+            origin: [true, 0.5],
+            transform: translate
+        });
+        //this._imageModifier.setTransform(Transform.multiply(translate,scale));
+
+        this._imageSurface.on('click', function () {
+            this._inputSurface.setValue('');
+            this.eventOutput.emit('inputCleared');
+        }.bind(this));
+
+        this.mainNode = this.add(this.rootModifier);
+        this.mainNode.add(this._mainSurface);
+        this.mainNode.add(this._inputSurface);
+        this.mainNode.add(this._imageModifier).add(this._imageSurface);
+    }
+
+    ClearInput.prototype = Object.create(View.prototype);
+    ClearInput.prototype.constructor = ClearInput;
+
+    ClearInput.prototype.getValue = function () {
+        return this._inputSurface.getValue();
+    };
+
+    ClearInput.prototype.setValue = function (data) {
+        return this._inputSurface.setValue(data);
+    };
+
+    ClearInput.prototype.ev = function (ev, action) {
+        this._inputSurface.on(ev, action);
+    };
+
+    ClearInput.prototype.focus = function() {
+        this._inputSurface.focus();
+    };
+
+    var defaultClearImg = '';
+
+    module.exports = ClearInput;
+});
  *   The MIT License (MIT)
  *
  *   Copyright (c) 2014 Motley Agency
